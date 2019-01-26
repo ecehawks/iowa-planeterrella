6c15bdf9182b3bb4c0ef4309c6b589f8fc8d8b86
Jordan Pohlman <jordanpohlman@gmail.com>
2019-01-26 10:06:06 -0600

    dependencies and new structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
+import LandingPage from './LandingPage';
+import Footer from './Footer';
 
 class App extends Component {
 
@@ -39,20 +41,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar/>
+        <div className="planeterrella-banner">
+          <div className="planeterrella-img"></div>
+        </div>
+        <LandingPage/>
+        
+
+        
         <p>{this.state.response}</p>
         <form onSubmit={this.handleSubmit}>
           <p>
@@ -66,6 +62,7 @@ class App extends Component {
           <button type="submit">Submit</button>
         </form>
         <p>{this.state.responseToPost}</p>
+        <Footer/>
       </div>
     );
   }

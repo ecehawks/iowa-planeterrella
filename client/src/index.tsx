35dabe5f0f22af1ae3493f65206f07593a2beb81
Jordan Pohlman <jordanpohlman@gmail.com>
2019-01-29 17:22:13 -0600

    add switch to browser router and add routing to App

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom'
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<HashRouter>
+	<BrowserRouter>
 		<Route path='' component={App}/>
-	</HashRouter>, 
+	</BrowserRouter>, 
 	document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

723101b8b22139eb558936c2008ef18e67628ba3
Nick Dallege <nick-dallege@uiowa.edu>
2019-06-06 19:32:44 -0500

    building things

---
+++ build/precache-manifest.3b91eca5618866ad3009d459bd5744a0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2e8bedb12cef56da0ade3222069674f",
+    "url": "./static/media/ring_current (1).c2e8bedb.jpg"
+  },
+  {
+    "revision": "51aa7d80ad7c2cd09e0b",
+    "url": "./static/css/main.e3fb6d5d.chunk.css"
+  },
+  {
+    "revision": "f497807feb09566d847dce29dd88b9d6",
+    "url": "./static/media/nsf_logo.f497807f.png"
+  },
+  {
+    "revision": "f34d497b798ccea9ecc2",
+    "url": "./static/js/1.f34d497b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4e90c596ab5c1421a1fa5c02ff2a4554",
+    "url": "./static/media/auroroa.4e90c596.jpg"
+  },
+  {
+    "revision": "c6b6b9e72469ae4c52bc172c55217d46",
+    "url": "./static/media/auroroa (1).c6b6b9e7.jpg"
+  },
+  {
+    "revision": "d58ed1519bade166b6bf3bfd3798488a",
+    "url": "./static/media/birkeland.d58ed151.jpg"
+  },
+  {
+    "revision": "02e25e015d57061fa624fd3a369b9c83",
+    "url": "./static/media/ring_current.02e25e01.jpg"
+  },
+  {
+    "revision": "51aa7d80ad7c2cd09e0b",
+    "url": "./static/js/main.51aa7d80.chunk.js"
+  },
+  {
+    "revision": "ce42d2ec1a7233fd642d924a8848f542",
+    "url": "./static/media/radiation_belt.ce42d2ec.jpg"
+  },
+  {
+    "revision": "feba72c999571e0e296b3036c8a877c1",
+    "url": "./static/media/radiation_belt (1).feba72c9.jpg"
+  },
+  {
+    "revision": "5d063af551a4d5c636a544276deba1d4",
+    "url": "./static/media/10712677_372448922931609_40_med.5d063af5.jpeg"
+  },
+  {
+    "revision": "9ca4afb8392a424571eea0b9ec72deac",
+    "url": "./static/media/fractal-1280079_960_720.9ca4afb8.jpg"
+  },
+  {
+    "revision": "ada69b34b6d79415684eee088a825b8a",
+    "url": "./static/media/DomeWordSingle GOLD-ffcd00.ada69b34.png"
+  },
+  {
+    "revision": "c49261cca5fe8030f39fe119bab6950c",
+    "url": "./static/media/planeterrella_banner.c49261cc.jpg"
+  },
+  {
+    "revision": "80f20e071664d99ef63b28c3eed18460",
+    "url": "./static/media/logo_gold.80f20e07.png"
+  },
+  {
+    "revision": "f34d497b798ccea9ecc2",
+    "url": "./static/css/1.9214974d.chunk.css"
+  },
+  {
+    "revision": "427a33e6d652125cfd0502750d9e431f",
+    "url": "./index.html"
+  }
+];
